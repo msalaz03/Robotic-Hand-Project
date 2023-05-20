670da9822886192e199c642c1db09f8389c81f1f
Matthew S <116814238+msalaz03@users.noreply.github.com>
2023-05-20 14:04:58 -0400

    Update Receiver-Test.cpp

--- Receiver-Test.cpp
+++ Receiver-Test.cpp
@@ -1,30 +1,25 @@
 //Receiver Code
 
-#define CE 7
-#define CSN 8
-
 #include <SPI.h>
-#include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "00001";
+RF24 radio(9, 10);  // CE, CSN
+
+const uint64_t address = 0xABCDABCDABCDLL; // Address between the transmitter and receiver
 
 void setup() {
   Serial.begin(9600);
   radio.begin();
-  radio.openReadingPipe(0, address);
+  radio.openReadingPipe(1, address);
   radio.setPALevel(RF24_PA_LOW);
   radio.startListening();
 }
 
 void loop() {
-
   if (radio.available()) {
     char text[32] = "";
     radio.read(&text, sizeof(text));
+    Serial.print("Received message: ");
     Serial.println(text);
-    
   }
- 
 }

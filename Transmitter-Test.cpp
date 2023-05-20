85f4da2220bf53b27775ee3066571a690bc615e1
Matthew S <116814238+msalaz03@users.noreply.github.com>
2023-05-20 14:57:07 -0400

    Update Transmitter-Test.cpp

--- Transmitter-Test.cpp
+++ Transmitter-Test.cpp
@@ -1,20 +1,20 @@
+#include <nRF24L01.h>
 #include <SPI.h>
 #include <RF24.h>
 
-RF24 radio(9, 10);  // CE, CSN
+RF24 radio(7, 8);  // CE, CSN
 
-const uint64_t address = 0xABCDABCDABCDLL; // Address between the transmitter and receiver
+const byte address[6] = "00001";
 
 void setup() {
-  Serial.begin(9600);
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_LOW);
+  radio.stopListening();
 }
 
 void loop() {
   const char text[] = "Hello World";
   radio.write(&text, sizeof(text));
-  Serial.println("Message sent: Hello World");
   delay(1000);
 }
